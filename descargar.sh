8b96808291ac8185350eb84d7b86485d39b0ad64
midiam1 <admhimmeros@gmail.com>
2022-01-06 21:26:36 -0400

    Update descargar.sh

--- descargar.sh
+++ descargar.sh
@@ -8,4 +8,14 @@
 
     # Esto incluye todos los requisitos previos para la construcción de KVM en Ubuntu:
 
-sudo apt-get install git apache2 apt-cacher-ng python-vm-builder ruby qemu-utils
+sudo apt -y install git apache2 apt-cacher-ng python-vm-builder ruby qemu-utils
+
+    # Instalo la base de la máquina virtual, para x86 y x64
+    # previa clonación de Gitian
+
+git clone https://github.com/devrandom/gitian-builder gitian-builder
+
+cd gitian-builder
+
+bin/make-base-vm
+bin/make-base-vm --arch i386
